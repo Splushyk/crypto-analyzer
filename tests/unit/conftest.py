0f75b3026a4989fbb9e4a0ef13cb14e188db98bf
decembra <foreverundersky@gmail.com>
2026-03-17 14:13:37 +0300

    test: reorganize test structure and consolidate fixtures in unit folder

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -17,6 +17,38 @@ def sample_coin():
 
 
 @pytest.fixture
+def gecko_raw_data():
+    return [
+        {
+            "name": "SomeCoin",
+            "symbol": "SC",
+            "current_price": 10.12345,
+            "price_change_percentage_24h": 8.12345,
+            "total_volume": 555.12345,
+            "market_cap": 777.12345,
+        }
+    ]
+
+
+@pytest.fixture
+def cmc_raw_data():
+    return [
+        {
+            "name": "SomeCoin",
+            "symbol": "SC",
+            "quote": {
+                "USD": {
+                    "price": 10.12345,
+                    "percent_change_24h": 8.12345,
+                    "volume_24h": 555.12345,
+                    "market_cap": 777.12345,
+                }
+            }
+        }
+    ]
+
+
+@pytest.fixture
 def api_client():
     client = ApiClient(base_url="https://fake-api.com")
     return client
