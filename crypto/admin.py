1a50b52e246ce215d1e76f0e9a5114613cbe8016
decembra <foreverundersky@gmail.com>
2026-03-29 19:54:00 +0300

    feat: register models in admin and configure list_display

--- crypto/admin.py
+++ crypto/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Snapshot, CoinPrice
 
-# Register your models here.
+
+@admin.register(Snapshot)
+class SnapshotAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at', 'total_market_cap')
+    ordering = ('-created_at',)
+
+
+@admin.register(CoinPrice)
+class CoinPriceAdmin(admin.ModelAdmin):
+    list_display = ('symbol', 'price', 'change_24h', 'snapshot')
+    list_filter = ('symbol', 'snapshot')
+    search_fields = ('symbol', 'name')
