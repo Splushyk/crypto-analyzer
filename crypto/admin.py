72e7d6f2b3c93660b518232ee8fc1a1ccbc97567
decembra <foreverundersky@gmail.com>
2026-03-30 08:53:38 +0300

    feat: add TabularInline for CoinPrice in Snapshot admin

--- crypto/admin.py
+++ crypto/admin.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 from .models import Snapshot, CoinPrice
 
 
+class CoinPriceInline(admin.TabularInline):
+    model = CoinPrice
+    extra = 0  # Чтобы Django не создавал пустые строки для новых записей по умолчанию
+    readonly_fields = ('symbol', 'price', 'change_24h')
+
+
 @admin.register(Snapshot)
 class SnapshotAdmin(admin.ModelAdmin):
     list_display = ('id', 'created_at', 'total_market_cap')
     ordering = ('-created_at',)
+    inlines = [CoinPriceInline]
 
 
 @admin.register(CoinPrice)
