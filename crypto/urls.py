c7e6b16e7e15d071758b05f9d674734c369ab3fb
decembra <foreverundersky@gmail.com>
2026-04-07 12:17:29 +0300

    feat: implement coin price history for all snapshots

--- crypto/urls.py
+++ crypto/urls.py
@@ -1,8 +1,11 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from crypto.views import SnapshotViewSet
+from crypto.views import SnapshotViewSet, CoinPriceHistoryView
 
 router = DefaultRouter()
 router.register(r'snapshots', SnapshotViewSet)
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('coins/', CoinPriceHistoryView.as_view()),
+]
--- crypto/views.py
+++ crypto/views.py
@@ -1,10 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
-from crypto.models import Snapshot
-from crypto.serializers import SnapshotSerializer
+from crypto.models import Snapshot, CoinPrice
+from crypto.serializers import SnapshotSerializer, CoinPriceSerializer
 
 
 class SnapshotViewSet(viewsets.ModelViewSet):
     queryset = Snapshot.objects.all()
     serializer_class = SnapshotSerializer
     http_method_names = ['get', 'head', 'options']
+
+
+class CoinPriceHistoryView(generics.ListAPIView):
+    def get_queryset(self):
+        symbol = self.request.query_params.get('symbol')
+        return CoinPrice.objects.filter(symbol=symbol)
+
+    serializer_class = CoinPriceSerializer
