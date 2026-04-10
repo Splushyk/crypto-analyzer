c8b234d82e02897db1f8be959b04074ec2ba3998
decembra <foreverundersky@gmail.com>
2026-04-10 16:28:54 +0300

    feat: add watchlist URL routes

--- crypto/urls.py
+++ crypto/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from crypto.views import SnapshotViewSet, CoinPriceHistoryView
+from crypto.views import SnapshotViewSet, CoinPriceHistoryView, WatchlistView, WatchlistDetailView
 
 router = DefaultRouter()
 router.register(r'snapshots', SnapshotViewSet)
 
 urlpatterns = router.urls + [
     path('coins/', CoinPriceHistoryView.as_view()),
+    path('watchlist/', WatchlistView.as_view()),
+    path('watchlist/<str:symbol>/', WatchlistDetailView.as_view()),
 ]
