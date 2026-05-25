524027030496a1ef18cf2676e7a3fb2d5976c87b
decembra <foreverundersky@gmail.com>
2026-05-25 15:01:51 +0300

    fix(observability): wrap DB backend for Prometheus query metrics

--- config/settings/base.py
+++ config/settings/base.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django_prometheus.db.backends.postgresql",
         "NAME": env("DB_NAME"),
         "USER": env("DB_USER"),
         "PASSWORD": env("DB_PASSWORD"),
